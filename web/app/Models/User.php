1184e70bebcd0251de92b5dab7413fafca490346
nik-kovacic <nik.kovacic@agiledrop.com>
2024-03-05 14:07:51 +0100

    Pint corrections

--- web/app/Models/User.php
+++ web/app/Models/User.php
@@ -10,7 +10,6 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Log;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements FilamentUser
