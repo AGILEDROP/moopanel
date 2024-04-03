7fe110dcd07b0d292ef3be6e9c3d65c1e9e754ba
nik-kovacic <nik.kovacic@agiledrop.com>
2024-04-03 14:37:12 +0200

    Implement toggle table layout action for the ListInstance page

--- web/app/Providers/Filament/AdminPanelProvider.php
+++ web/app/Providers/Filament/AdminPanelProvider.php
@@ -3,14 +3,15 @@
 namespace App\Providers\Filament;
 
 use App\Filament\Auth\Login;
+use App\Filament\Resources\InstanceResource\Pages\CreateInstance;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Widgets;
+use Hydrat\TableLayoutToggle\TableLayoutTogglePlugin;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -38,14 +39,21 @@ class AdminPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->discoverClusters(in: app_path('Filament/Clusters'), for: 'App\\Filament\\Clusters')
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->viteTheme('resources/css/app.css')
             ->pages([
-                Pages\Dashboard::class,
+                //                CreateInstance::class,
+                //                Pages\Dashboard::class,
             ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
+            ])
+            ->plugins([
+                TableLayoutTogglePlugin::make()
+                    ->persistLayoutInLocalStorage(false) // allow user to keep his layout preference in his local storage
+                    ->shareLayoutBetweenPages(false) // allow all tables to share the layout option (requires persistLayoutInLocalStorage to be true)
+                    ->displayToggleAction(false), // used to display the toggle button automatically, on the desired filament hook (defaults to table bar),
             ])
             ->middleware([
                 EncryptCookies::class,
