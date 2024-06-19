cbff485805d2b190c72b742107eeb67890339424
Kristof Fabjan <kristof.fabjan@agiledrop.com>
2024-06-19 14:28:20 +0200

    feat: Update requests and update request items lists
    Added widget with update request list in instance dashboard. On click, it shows page with update request items list
    
    Tags: update-request, update-request-items, list, widget, page

--- web/app/Models/UpdateRequestItem.php
+++ web/app/Models/UpdateRequestItem.php
@@ -2,12 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UpdateRequestItem extends Model
 {
     use HasFactory;
+
+    public const STATUS_PENDING = null;
+    public const STATUS_SUCCESS = true;
+    public const STATUS_FAIL = false;
 
     protected $fillable = [
         'update_request_id',
@@ -20,6 +25,20 @@ class UpdateRequestItem extends Model
         'zip_path',
         'error'
     ];
+
+    protected function statusName(): Attribute
+    {
+        return Attribute::make(
+            get: function () {
+                return match ($this->status) {
+                    self::STATUS_PENDING => __('Pending'),
+                    self::STATUS_SUCCESS => __('Success'),
+                    self::STATUS_FAIL => __('Fail'),
+                    default => __('Unknown'),
+                };
+            }
+        );
+    }
 
     public function update_request()
     {
