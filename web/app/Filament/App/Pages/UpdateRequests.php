058861cffe9277cd913b90c932c3e8ab4f3cabe4
Kristof Fabjan <kristof.fabjan@agiledrop.com>
2024-06-26 10:35:56 +0200

    feat: Dynamic table render depending on updaterequest type showing udpate request items data
    Conditionally toggle columns that dont contain data in update request items  list. Conditionally toggle based on update request type(plugin_update, zip_update, core)
    
    Tags: table, update-request-items

--- web/app/Filament/App/Pages/UpdateRequests.php
+++ web/app/Filament/App/Pages/UpdateRequests.php
@@ -9,6 +9,7 @@ use Filament\Support\Enums\FontWeight;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
+use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
@@ -21,21 +22,32 @@ class UpdateRequests extends Page implements HasTable
 
     protected static string $view = 'filament.app.pages.update-requests';
 
-    protected static ?string $title = 'Update Request items';
-
     public ?int $updateRequestId = null;
 
+    public ?UpdateRequest $updateRequest = null;
+
     protected static bool $shouldRegisterNavigation = false;
+
+    public function getTitle(): string | Htmlable
+    {
+        $title = __('Update Request items');
+
+        if ($this->updateRequest && !empty($this->updateRequest->name)) {
+            $title = $this->updateRequest->name . __(' - update request items');
+        }
+
+        return $title;
+    }
 
     public function mount(): void
     {
         // Render update request items
-        if (! empty(request()->query('id', ''))) {
+        if (!empty(request()->query('id', ''))) {
             $updateRequestId = (int) request()->query('id', '');
-            $updateRequest = UpdateRequest::find($updateRequestId);
+            $this->updateRequest = UpdateRequest::find($updateRequestId);
 
             // Check if the user is the owner of the update request
-            if (Auth::user()->id === $updateRequest->user_id) {
+            if (Auth::user()->id === $this->updateRequest->user_id) {
                 $this->updateRequestId = $updateRequestId;
 
                 return;
@@ -61,20 +73,28 @@ class UpdateRequests extends Page implements HasTable
 
     protected function getTableColumns(): array
     {
-        // TODO: hide some columns if necessary
-        return [
+        $updateRequest = UpdateRequest::find($this->updateRequestId);
+
+        // Column array manual generation because of dependency injection issues in hidden() method of filament
+        $columns = [
             TextColumn::make('statusName')
                 ->label(__('Status'))
                 ->color(fn (Model $model) => is_null($model->status) ? 'warning' : ($model->status ? 'success' : 'danger'))
                 ->badge(),
             TextColumn::make('component')
                 ->weight(FontWeight::Bold),
-            TextColumn::make('model_id')
-                ->label(__('UpdateID')),
             TextColumn::make('version'),
-            TextColumn::make('release'),
-            TextColumn::make('error')
-                ->label(__('Message')),
         ];
+
+        if ($updateRequest && $updateRequest->type !== UpdateRequest::TYPE_PLUGIN_ZIP) {
+            $columns[] = TextColumn::make('model_id')
+                ->label(__('UpdateID'));
+            $columns[] = TextColumn::make('release');
+        }
+
+        $columns[] = TextColumn::make('error')
+            ->label(__('Message'));
+
+        return $columns;
     }
 }
